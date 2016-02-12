4ada2b4e149298a3be2f19d806a69577a57e32b5
jwhiddon <jwhiddon@gmail.com>
2016-02-12 14:58:26 -0700

    Update AT24C1024.h

--- AT24C1024.h
+++ AT24C1024.h
@@ -1,47 +1,9 @@
 #ifndef AT24C1024_h
 #define AT24C1024_h
 /*
-  AT24C1024.h
-  AT24C1024 Library for Arduino 
-  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  US
-
-  This library is based on several projects:
- 
-  The Arduino EEPROM library found here:  
-  http://arduino.cc/en/Reference/EEPROM
-
-  The 24C256 library found here:
-  http://www.arduino.cc/playground/Code/I2CEEPROM
-
-  The 24C512 library found here:
-  http://www.arduino.cc/playground/Code/I2CEEPROM24LC512
-
-  Our project page is here:
+  AT24C1024.cpp
+  AT24C1024 EEPROM Library for Arduino 
   http://www.arduino.cc/playground/Code/I2CEEPROM24C1024
-  
-  From the datasheet:
-
-  The AT24C1024B provides 1,048,576 bits of serial electrically 
-  erasable and programmable read only memory (EEPROM) organized 
-  as 131,072 words of 8 bits each. The device’s cascadable 
-  feature allows up to four devices to share a common two-wire 
-  bus.
-  
-  http://www.atmel.com/dyn/resources/prod_documents/doc5194.pdf
-  
 */
 
 #ifdef ARDUINO >= 100
@@ -54,7 +16,6 @@ extern "C" {
 
 #include <Wire.h>
 
-// Settings
 #define FULL_MASK 0x7FFFF
 #define DEVICE_MASK 0x7F0000
 #define WORD_MASK 0xFFFF
