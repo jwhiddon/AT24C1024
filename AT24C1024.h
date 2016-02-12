026ec2767f87e8339237361efc53e565754fe8c6
jwhiddon <jwhiddon@gmail.com>
2016-02-12 14:55:10 -0700

    Update AT24C1024.h

--- AT24C1024.h
+++ AT24C1024.h
@@ -1,4 +1,4 @@
-﻿#ifndef AT24C1024_h
+#ifndef AT24C1024_h
 #define AT24C1024_h
 /*
   AT24C1024.h
@@ -54,9 +54,11 @@ extern "C" {
 
 #include <Wire.h>
 
+// Settings
 #define FULL_MASK 0x7FFFF
 #define DEVICE_MASK 0x7F0000
 #define WORD_MASK 0xFFFF
+
 class AT24C1024
 {
   public:
