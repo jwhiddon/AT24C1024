3f24ed160854b5aa405678158de5876bf0fbc375
jwhiddon <jwhiddon@gmail.com>
2016-02-12 11:04:20 -0700

    Update E24C1024.cpp

--- E24C1024.cpp
+++ E24C1024.cpp
@@ -43,6 +43,7 @@
 */
 
 #include <Wire.h>
+#include <Arduino.h>
 #include "E24C1024.h"
 
 E24C1024::E24C1024(void)
@@ -68,7 +69,7 @@ uint8_t E24C1024::read(unsigned long dataAddress)
    Wire.send((uint8_t)(dataAddress & 0xFF)); // LSB
    Wire.endTransmission();
    Wire.requestFrom((uint8_t)((0x500000 | dataAddress) >> 16),1);
-   if (Wire.available()) data = Wire.receive();
+   if (Wire.available()) data = Wire.peek();
    return data;
 }
 
